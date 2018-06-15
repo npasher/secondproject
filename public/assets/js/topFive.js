af44406cd54fcebfa91b85c432dc7917e0e7bf3d
npasher <n.p.asher@gmail.com>
2018-06-15 15:36:24 -0400

    work in pro

--- public/assets/js/topFive.js
+++ public/assets/js/topFive.js
@@ -2,8 +2,8 @@ $.get("/api/topfive",function(data){
   for (let i=0;i<5;i++){
   $("#table").html(
     `<h6 class="action-title text-center">Top Scores:<h6>
-    <p>Name:${data[i]}</p>
-    <p>Wins:${data[i]}</p>`
+    <p>Name:${data[i].userName}</p>
+    <p>Wins:${data[i].wins}</p>`
   )
   console.log(data);
 }
