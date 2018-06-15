34e06c69aeebd0e6255d13707c6eb0d45a655daf
npasher <n.p.asher@gmail.com>
2018-06-15 11:15:05 -0400

    cleaning

--- server.js
+++ server.js
@@ -1,43 +1,37 @@
-// *****************************************************************************
-// Server.js - This file is the initial starting point for the Node/Express server.
-//
-// ******************************************************************************
-// *** Dependencies
-// =============================================================
+//Server.js is the initial point for the Node/Express server.//
+
+//Dependencies.//
 const express=require("express");
 const bodyParser=require("body-parser");
 const env=require('dotenv');
 const path = require('path');
-// Sets up the Express App
-// =============================================================
+//Setting up Express App.//
 const app=express();
 const PORT=process.env.PORT || 7000;
 
-// Requiring our models for syncing
+//Requiring models for syncing.//
 const db=require("./models");
 
-// Sets up the Express app to handle data parsing
+//Sets up the Express app to handle data parsing.//
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended:true}));
 // parse application/json
 app.use(bodyParser.json());
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-// Routes
-// =============================================================
+// Routes.//
 require("./routes/html-routes.js")(app);
 require("./routes/user-api-routes.js")(app);
 
 // Static directory/this should come after our routes
 console.log(__dirname);
-app.use('/public', express.static("public"));
+app.use('/public',express.static("public"));
 
-// Syncing our sequelize models and then starting our Express app
-// =============================================================
-db.sequelize.sync({force: false}).then(function(){
-  app.listen(PORT, function() {
+//Syncing sequelize models and starting Express app.//
+db.sequelize.sync({force:false}).then(function(){
+  app.listen(PORT,function(){
     console.log("App listening on PORT " + PORT);
   });
 });
